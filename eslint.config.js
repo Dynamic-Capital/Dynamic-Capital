24161b1370038a5d82785be4e2eb9e7a0f1bf892
Dynamic-Capital <deathcoder8@gmail.com>
2025-09-08 22:59:52 +0500

    feat: add Next.js ESLint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import nextPlugin from "@next/eslint-plugin-next";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -14,10 +15,12 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      "@next/next": nextPlugin,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
+      ...nextPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": "off",
       "@typescript-eslint/no-unused-vars": "off",
