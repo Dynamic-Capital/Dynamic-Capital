2597faa80247a071a0f20d38b5ed6178cc17ec6a
Dynamic-Capital <deathcoder8@gmail.com>
2025-09-08 16:28:26 +0500

    chore: avoid Supabase init crash

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { SUPABASE_ENV_ERROR } from "@/config/supabase";
 import EnvError from "@/components/EnvError";
 
-const root = document.getElementById("root")!;
-createRoot(root).render(
-  SUPABASE_ENV_ERROR ? <EnvError message={SUPABASE_ENV_ERROR} /> : <App />,
-);
+const rootEl = document.getElementById("root")!;
+const root = createRoot(rootEl);
+
+if (SUPABASE_ENV_ERROR) {
+  root.render(<EnvError message={SUPABASE_ENV_ERROR} />);
+} else {
+  import("./App.tsx")
+    .then(({ default: App }) => root.render(<App />))
+    .catch((err) => root.render(<EnvError message={String(err)} />));
+}
