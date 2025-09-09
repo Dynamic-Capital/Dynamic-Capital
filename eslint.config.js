d113e89ac0c5cc0af9e14dc503e6e23cbc989282
Dynamic-Capital <deathcoder8@gmail.com>
2025-09-09 15:28:33 +0500

    chore: use Next.js ESLint recommended config

--- eslint.config.js
+++ eslint.config.js
@@ -8,19 +8,21 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist", "tests/**", "**/vendor/**"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      nextPlugin.flatConfig.recommended,
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      "@next/next": nextPlugin,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...nextPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": "off",
       "@typescript-eslint/no-unused-vars": "off",
