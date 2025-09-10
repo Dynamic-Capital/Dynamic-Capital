6854c17f08ef30a6f34fdbd1f531dd721087195e
Dynamic-Capital <deathcoder8@gmail.com>
2025-09-10 21:31:17 +0500

    chore: add HTTP response helper

---
+++ lib/http.ts
@@ -0,0 +1,13 @@
+export function jsonResponse(data: unknown, init: ResponseInit = {}) {
+  return new Response(JSON.stringify(data), {
+    ...init,
+    headers: {
+      'content-type': 'application/json',
+      ...(init.headers || {}),
+    },
+  });
+}
+
+export function methodNotAllowed() {
+  return jsonResponse({ error: 'Method Not Allowed' }, { status: 405 });
+}
