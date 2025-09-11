fc5b1b1f29214a2c77735376f42ab30e8db27196
Dynamic-Capital <deathcoder8@gmail.com>
2025-09-11 11:59:54 +0500

    Add basic Pages Router structure

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import "../styles/globals.css";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <p>Welcome to the home page.</p>
+      </main>
+    </>
+  );
+}
