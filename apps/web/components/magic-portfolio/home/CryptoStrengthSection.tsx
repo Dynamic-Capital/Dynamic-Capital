a28967a43b423802f879a0b6f63da81e03c3d47a
Dynamic-Capital <deathcoder8@gmail.com>
2025-09-26 10:05:00 +0500

    Refactor heatmap tool for unified multi-asset data

---
+++ apps/web/components/magic-portfolio/home/CryptoStrengthSection.tsx
@@ -0,0 +1,7 @@
+import { HeatmapTool } from "@/components/tools/HeatmapTool";
+
+export function CryptoStrengthSection() {
+  return <HeatmapTool id="crypto-strength" assetClass="crypto" />;
+}
+
+export default CryptoStrengthSection;
