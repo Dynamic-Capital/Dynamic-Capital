bdf349e6349fa8372d98193fa3cbdf16bdbe17e3
Dynamic-Capital <deathcoder8@gmail.com>
2025-09-26 13:07:03 +0500

    feat: add TonConnect theme loader

---
+++ apps/web/lib/miniapp-theme.ts
@@ -0,0 +1,18 @@
+export {
+  MiniAppThemeManager,
+  attachGlobalTonConnect,
+  attachTonConnect,
+  getMiniAppThemeManager,
+} from "../../../shared/miniapp/theme-loader";
+export type {
+  MiniAppThemeOption,
+  MiniAppThemeState,
+  ThemeAccessibilityPair,
+  ThemeFontDefinition,
+  ThemeSoundDefinition,
+  TonConnectLike,
+} from "../../../shared/miniapp/theme-loader";
+export {
+  useMiniAppThemeManager,
+  type UseMiniAppThemeResult,
+} from "../../../shared/miniapp/use-miniapp-theme";
