1b4694f8d66d239fc9fc88a8ff9c968062835676
Dynamic Capital <deathcoder8@gmail.com>
2025-09-27 22:59:34 +0500

    feat: add dynamic reference intelligence module

---
+++ dynamic_reference/__init__.py
@@ -0,0 +1,15 @@
+"""Dynamic reference intelligence toolkit."""
+
+from .reference import (
+    DynamicReference,
+    ReferenceContext,
+    ReferenceDigest,
+    ReferenceEntry,
+)
+
+__all__ = [
+    "DynamicReference",
+    "ReferenceContext",
+    "ReferenceDigest",
+    "ReferenceEntry",
+]
