8f7125749f00fa66c65e606f34ad352d458f86d1
Dynamic Capital <deathcoder8@gmail.com>
2025-09-28 00:45:51 +0500

    feat: add dynamic domain planner

---
+++ dynamic_domain/__init__.py
@@ -0,0 +1,17 @@
+"""Domain planning toolkit for Dynamic Capital deployments."""
+
+from .manager import (
+    DomainChange,
+    DomainChangePlan,
+    DomainChangeType,
+    DomainRecord,
+    DynamicDomainManager,
+)
+
+__all__ = [
+    "DomainChange",
+    "DomainChangePlan",
+    "DomainChangeType",
+    "DomainRecord",
+    "DynamicDomainManager",
+]
