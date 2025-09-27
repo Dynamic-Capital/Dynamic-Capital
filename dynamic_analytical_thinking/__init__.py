b7c7433f7e02ce2f144b25021c24280bfe7ee7fd
Dynamic Capital <deathcoder8@gmail.com>
2025-09-27 21:32:02 +0500

    Add dynamic analytical thinking engine

---
+++ dynamic_analytical_thinking/__init__.py
@@ -0,0 +1,15 @@
+"""Dynamic analytical thinking engine for evidence synthesis."""
+
+from .engine import (
+    AnalyticalContext,
+    AnalyticalInsight,
+    AnalyticalSignal,
+    DynamicAnalyticalThinking,
+)
+
+__all__ = [
+    "AnalyticalContext",
+    "AnalyticalInsight",
+    "AnalyticalSignal",
+    "DynamicAnalyticalThinking",
+]
