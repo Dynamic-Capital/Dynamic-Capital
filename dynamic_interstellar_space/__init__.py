8f3f77644420d54179b7223d83d0219f3b34060c
Dynamic Capital <deathcoder8@gmail.com>
2025-09-28 01:11:30 +0500

    feat: create dynamic interstellar space module

---
+++ dynamic_interstellar_space/__init__.py
@@ -0,0 +1,19 @@
+"""Interstellar space orchestration primitives for Dynamic Capital."""
+
+from .space import (
+    CosmicCurrent,
+    DynamicInterstellarSpace,
+    InterstellarEvent,
+    StellarBody,
+    StellarCoordinate,
+    WormholeCorridor,
+)
+
+__all__ = [
+    "CosmicCurrent",
+    "DynamicInterstellarSpace",
+    "InterstellarEvent",
+    "StellarBody",
+    "StellarCoordinate",
+    "WormholeCorridor",
+]
