d4b5750e0fd20be0d4edc1f5e16df4a36d260ee0
Dynamic Capital <deathcoder8@gmail.com>
2025-09-28 02:13:18 +0500

    Add dynamic proof-of-authority consensus module

---
+++ dynamic_proof_of_authority/__init__.py
@@ -0,0 +1,9 @@
+"""Dynamic Proof of Authority (PoA) consensus utilities."""
+
+from .poa import Authority, AuthorityBlock, DynamicProofOfAuthority
+
+__all__ = [
+    "Authority",
+    "AuthorityBlock",
+    "DynamicProofOfAuthority",
+]
