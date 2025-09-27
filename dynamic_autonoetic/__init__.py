3e6871c82749064ed124680b3c87a771d049759d
Dynamic Capital <deathcoder8@gmail.com>
2025-09-27 21:26:17 +0500

    Add autonoetic consciousness engine

---
+++ dynamic_autonoetic/__init__.py
@@ -0,0 +1,15 @@
+"""Autonoetic consciousness utilities for Dynamic Capital rituals."""
+
+from .consciousness import (
+    AutonoeticConsciousness,
+    AutonoeticContext,
+    AutonoeticSignal,
+    AutonoeticState,
+)
+
+__all__ = [
+    "AutonoeticConsciousness",
+    "AutonoeticContext",
+    "AutonoeticSignal",
+    "AutonoeticState",
+]
