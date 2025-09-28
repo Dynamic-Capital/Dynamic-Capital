d3c055b395ab168c68204de54b4b436c257e6bac
Dynamic Capital <deathcoder8@gmail.com>
2025-09-28 05:55:01 +0500

    Optimize metric aggregation

--- dynamic_agi/__init__.py
+++ dynamic_agi/__init__.py
@@ -1,5 +1,19 @@
 """Dynamic AGI package exposing orchestrator utilities."""
 
 from .model import AGIDiagnostics, AGIOutput, DynamicAGIModel
+from .self_improvement import (
+    DynamicSelfImprovement,
+    ImprovementPlan,
+    ImprovementSignal,
+    LearningSnapshot,
+)
 
-__all__ = ["AGIDiagnostics", "AGIOutput", "DynamicAGIModel"]
+__all__ = [
+    "AGIDiagnostics",
+    "AGIOutput",
+    "DynamicAGIModel",
+    "DynamicSelfImprovement",
+    "ImprovementPlan",
+    "ImprovementSignal",
+    "LearningSnapshot",
+]
