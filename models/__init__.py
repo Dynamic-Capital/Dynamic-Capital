42b3a4951f34162c63e23a46552ec81935886736
Dynamic Capital <deathcoder8@gmail.com>
2025-09-28 15:27:33 +0500

    Improve holistic intelligence signal validation

---
+++ models/__init__.py
@@ -0,0 +1,21 @@
+"""Convenience exports for Dynamic Capital holistic intelligence models."""
+
+from .holistic_intelligence import (
+    DynamicAGIOrchestrator,
+    DynamicCognitiveModel,
+    DynamicHolisticIntelligence,
+    DynamicSelfAwarenessModel,
+    DynamicThinkingModel,
+    DynamicUltimateRealityModel,
+    HolisticIntelligenceReport,
+)
+
+__all__ = [
+    "DynamicAGIOrchestrator",
+    "DynamicThinkingModel",
+    "DynamicSelfAwarenessModel",
+    "DynamicCognitiveModel",
+    "DynamicUltimateRealityModel",
+    "DynamicHolisticIntelligence",
+    "HolisticIntelligenceReport",
+]
