b1fc943962f1f4591907fe4b7d9df490f083d014
Dynamic Capital <deathcoder8@gmail.com>
2025-09-28 15:18:41 +0500

    Add dynamic mass engine with stellar mass assignments

---
+++ dynamic_mass/__init__.py
@@ -0,0 +1,17 @@
+"""Dynamic mass modelling utilities for stellar systems."""
+
+from .mass import (
+    DynamicMassEngine,
+    SOLAR_MASS_KG,
+    SpectralType,
+    StarProfile,
+    StellarMassModel,
+)
+
+__all__ = [
+    "DynamicMassEngine",
+    "SOLAR_MASS_KG",
+    "SpectralType",
+    "StarProfile",
+    "StellarMassModel",
+]
