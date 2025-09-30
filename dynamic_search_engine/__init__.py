9cdefb832f417f8fdf7684bed7fd807733c45a7c
Dynamic Capital <deathcoder8@gmail.com>
2025-09-30 13:32:31 +0500

    feat: add dynamic search engine module

---
+++ dynamic_search_engine/__init__.py
@@ -0,0 +1,5 @@
+"""A lightweight TF-IDF powered search engine with dynamic updates."""
+
+from .engine import Document, DynamicSearchEngine, SearchResult
+
+__all__ = ["Document", "DynamicSearchEngine", "SearchResult"]
