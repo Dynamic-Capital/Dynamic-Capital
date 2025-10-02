bc0ca96a4266533240932b120e75febf34dc836c
Dynamic Capital <deathcoder8@gmail.com>
2025-10-02 21:30:04 +0500

    feat: add ordinary matter modelling engine

---
+++ dynamic_ordinary_matter/__init__.py
@@ -0,0 +1,17 @@
+"""Dynamic ordinary matter modelling engine."""
+
+from .ordinary_matter import (
+    BARYON_MASS_KG,
+    MatterConstituent,
+    OrdinaryMatterEngine,
+    OrdinaryMatterPhase,
+    OrdinaryMatterProfile,
+)
+
+__all__ = [
+    "BARYON_MASS_KG",
+    "MatterConstituent",
+    "OrdinaryMatterEngine",
+    "OrdinaryMatterPhase",
+    "OrdinaryMatterProfile",
+]
