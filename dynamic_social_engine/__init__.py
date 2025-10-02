2347e0f126e5fbffb0cafb6b0b845600a74f26ce
Dynamic Capital <deathcoder8@gmail.com>
2025-10-02 17:01:19 +0500

    feat: add dynamic social engine module

---
+++ dynamic_social_engine/__init__.py
@@ -0,0 +1,21 @@
+"""Dynamic social engine package."""
+
+from .engine import (
+    ChannelPerformance,
+    EngagementSnapshot,
+    ScheduledPost,
+    ScheduleContext,
+    SocialChannel,
+    SocialPost,
+    DynamicSocialEngine,
+)
+
+__all__ = [
+    "ChannelPerformance",
+    "EngagementSnapshot",
+    "ScheduledPost",
+    "ScheduleContext",
+    "SocialChannel",
+    "SocialPost",
+    "DynamicSocialEngine",
+]
