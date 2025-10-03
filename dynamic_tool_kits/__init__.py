0f79d5bb8a6fb05bdf862855574022bc5b1bf674
Dynamic Capital <deathcoder8@gmail.com>
2025-10-03 18:28:55 +0500

    Expose DynamicWisdomEngine in toolkit

--- dynamic_tool_kits/__init__.py
+++ dynamic_tool_kits/__init__.py
@@ -260,7 +260,12 @@ _TOOLKIT_EXPORTS: Dict[str, Tuple[str, ...]] = {
     ),
     "dynamic_ultimate_reality": ("NonDualContext", "UltimateRealitySignal", "UltimateRealityState"),
     "dynamic_volume": ("BookLevel", "VolumeAlert", "VolumeSnapshot", "VolumeThresholds"),
-    "dynamic_wisdom": ("WisdomContext", "WisdomFrame", "WisdomSignal"),
+    "dynamic_wisdom": (
+        "DynamicWisdomEngine",
+        "WisdomContext",
+        "WisdomFrame",
+        "WisdomSignal",
+    ),
 }
 
 
