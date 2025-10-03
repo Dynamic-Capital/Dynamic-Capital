c24fdf35ad05ed4b87c57705d28cad143d246e86
Dynamic Capital <deathcoder8@gmail.com>
2025-10-03 18:16:18 +0500

    Add dynamic self-learning engine

---
+++ dynamic_self_learning/__init__.py
@@ -0,0 +1,15 @@
+"""Dynamic self-learning orchestration toolkit."""
+
+from .engine import (
+    DynamicSelfLearning,
+    LearningContext,
+    LearningPlan,
+    LearningSignal,
+)
+
+__all__ = [
+    "DynamicSelfLearning",
+    "LearningContext",
+    "LearningPlan",
+    "LearningSignal",
+]
