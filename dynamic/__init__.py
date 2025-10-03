9bfd1c143683b0b68efc7c25e35afb6fea1a24d9
Dynamic Capital <deathcoder8@gmail.com>
2025-10-03 21:13:44 +0500

    Enable dynamic blockchain namespace

--- dynamic/__init__.py
+++ dynamic/__init__.py
@@ -1,6 +1,9 @@
 """Dynamic Capital consolidated namespace."""
 
+from . import blockchain
+
 __all__ = [
+    "blockchain",
     "platform",
     "governance",
     "intelligence",
---
+++ dynamic/blockchain/__init__.py
@@ -0,0 +1,17 @@
+"""Dynamic blockchain exports via the consolidated namespace."""
+
+from dynamic_blockchain import (
+    Block,
+    DelegateState,
+    DynamicBlockchain,
+    DynamicDelegatedProofOfStake,
+    Transaction,
+)
+
+__all__ = [
+    "Block",
+    "DelegateState",
+    "DynamicBlockchain",
+    "DynamicDelegatedProofOfStake",
+    "Transaction",
+]
